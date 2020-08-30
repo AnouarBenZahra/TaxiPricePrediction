452494bc009ef71cd587ec1c934dff7f16ef220f
Anouar BEN ZAHRA <33717910+AnouarBenZahra@users.noreply.github.com>
2020-08-30 18:35:28 +0200

    add taxi price prediction

---
+++ TaxiPricePrediction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaxiPricePrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
